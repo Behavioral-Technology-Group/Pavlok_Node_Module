0ecf5e5ae19c49d20f385951bb9596cd06c5e526
Maclyn Brandwein <maclyn@maclyn.me>
2016-04-12 17:52:26 -0400

    Early changes to remove passport.

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ var OAuth2Strategy = oauth.OAuth2Strategy;
 //Request to query API
 var request = require('request');
 
-
-
-
 //'Constants' (can be altered with the options bundle/init)
 var BASE_URL = "http://pavlok-mvp.herokuapp.com";
 var PORT = 3000;
@@ -31,29 +28,28 @@ var CLIENT_SECRET = null;
 var CALLBACK_URL = null;
 var ALERT_TEXT = "Sent from Node Module.";
 
+//Shared fields
+var isInited = false;
+var isServer = false;
+var app;
 
+//Fields for usage as server
+var successUrl = "/success";
+var failureUrl = "/failure";
 
-
-//Fields
+//Fields for usage as client
 var tokenFile = null; //A representation of the token file on disk
 var signingIn = null; //Are we signing in?
 var code = null; //The fetched auth code
 
-var app; //The representation of the program
-var server; //The Express server
-
-
-
-
-/** Support functions **/
 function log(msg){
     if(VERBOSE) console.log("[Pavlok API] " + msg);
 }
 
+function logErr(msg){
+	console.log("[Pavlok API/Error] " + msg);
+}
 
-
-
-/** Load the auth token from disk **/
 function createTokenFile(){
     try {
         var skeletonObject = {
@@ -87,63 +83,6 @@ function clearTokenFile(){
     }
 }
 
-try {
-    tokenFile = JSON.parse(fs.readFileSync(TOKEN_FILENAME, 'utf8'));
-} catch (e) {
-    try {
-		createTokenFile();
-		tokenFile = JSON.parse(fs.readFileSync(TOKEN_FILENAME, 'utf8'));
-	} catch (ignored) {} //Will happen on systems without file I/O access
-}
-
-if(tokenFile.token != null){
-    code = tokenFile.token;
-}
-
-
-
-
-/** Setup the Express server; used to handle OAuth2 results **/
-app = express();
-app.use(express.static(__dirname + '/public'));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(passport.session());
-app.get("/", 
-	function(request, result)
-	{
-    result.redirect("index.html");
-	});
-app.get("/done",
-	function(request, result)
-	{
-    result.redirect("done.html");
-    if(code != null) server.close();
-	});
-app.get("/error", 
-	function(request, result)
-	{
-    result.redirect("error.html");
-	});
-app.get("/auth/pavlok",
-    passport.authenticate("oauth2",
-    {
-        "session": false,
-        "failureRedirect": "/error"
-    }));
-app.get("/auth/pavlok/result",
-    passport.authenticate("oauth2", 
-    { 
-        "session": false,
-        "successRedirect": "/done",
-        "failureRedirect": "/error"
-    }));
-
-
-
-
-
 //Exports
 var exports = module.exports = {};
 
@@ -156,8 +95,9 @@ var exports = module.exports = {};
   @param {Object} options - Custom setup options. Accepts a port ("port",
 				 number), callback URL ("callbackUrl", string), a verbose 
 			    debugging option ("verbose", boolean), an option to save
-				the token ("save", boolean), and a default value for
-				alert text ("message", string).
+				the token ("save", boolean), a default value for
+				alert text ("message", string), and a custom filename for 
+				the token to be stored in ("tokenFile", string).
  **/
 exports.init = function(cId, cSecret, options){
 	if(cId !== undefined && cSecret != undefined && typeof cId == "string" 
@@ -165,53 +105,176 @@ exports.init = function(cId, cSecret, options){
 		CLIENT_ID = cId;
 		CLIENT_SECRET = cSecret;		
 
-		if(options == undefined) options = {};
+		if(options == undefined || typeof options != "object") options = {};
 
-		if(options.save != undefined && typeof options.save == "boolean")
-			SAVE = options.save;
-
-		if(options.apiUrl !== undefined && 
-			typeof options.callbackUrl == "string")
+		//Fields shared between both modes
+		if(options.apiUrl !== undefined && 	typeof options.apiUrl == "string")
 			BASE_URL = options.apiUrl;
-
-		if(options.port !== undefined && typeof options.port == "number") 
-            PORT = options.port;        
-        
-		if(options.callbackUrl !== undefined &&
-			typeof options.callbackUrl == "string")
-            CALLBACK_URL = options.callbackUrl;
         
 		if(options.verbose !== undefined && typeof options.verbose == "boolean")
             VERBOSE = options.verbose; 
 
 		if(options.message !== undefined && typeof options.message == "string")
 			ALERT_TEXT = options.message;
+
+		if(options.app != undefined && typeof options.app == "object"){
+			isServer = true;
+			app = app;
+
+			//Setup matching options
+			if(options.callbackUrl !== undefined && typeof options.callbackUrl == "string"){
+				CALLBACK_URL = options.callbackUrl;
+			} else {
+				logErr("callbackUrl must not be null for server apps; there is no default value!");
+				return;
+			}
+			
+			var configureSession = false;
+			if(options.configureSession != undefined &&
+				typeof options.configureSession == "boolean")
+				configureSession = options.configureSession;
+
+			var sessionSecret = "whywouldyouusethis";
+			if(options.sessionSecret != undefined &&
+				typeof options.sessionSecret == "string"){
+				sessionSecret = options.sessionSecret;
+			} else {
+				console.log("WARNING - Pavlok is configured as a server, but " +
+					"it is internally handling client token saving with a" +
+					" value that's insecure!");
+			}
+			
+			//Setup server to authenticate from the callback URL
+			
+		} else {
+			isServer = false;
+			//Setup matching fields
+
+			//Load fields
+			if(options.save != undefined && typeof options.save == "boolean")
+				SAVE = options.save;
+
+			if(options.port !== undefined && typeof options.port == "number") 
+			    PORT = options.port;
+			    
+			CALLBACK_URL = "http://localhost:" + PORT + "/auth/pavlok/result";
+
+			if(options.tokenFile !== undefined && typeof options.tokenFile == "string")
+				TOKEN_FILE = options.tokenFile;
+
+			//Load token file from the disk
+			try {
+				 tokenFile = JSON.parse(fs.readFileSync(TOKEN_FILENAME, 'utf8'));
+			} catch (e) {
+			    try {
+					createTokenFile();
+					tokenFile = JSON.parse(fs.readFileSync(TOKEN_FILENAME, 'utf8'));
+				} catch (ignored) {} //Will happen on systems without file I/O access
+			}
+
+			if(tokenFile.token != null){
+			    code = tokenFile.token;
+			}
+
+			//Setup app object
+			app = express();
+			app.use(express.static(__dirname + '/public'));
+			app.use(cookieParser());
+			app.use(bodyParser.json());
+			
+			app.get("/auth/pavlok", function(request, result){
+				result.redirect(BASE_URL + "/oauth/authorize?client_id" + CLIENT_ID + "&redirect_uri=" + CALLBACK_URL + "&response_type=code"); //Redirect to Pavlok server to authorize
+			});
+			app.get("/auth/pavlok/result", function(request, result){ //See if there's a code in here
+				if(request.query.code == null || request.query.code.length == 0){
+					result.send("You've rejected the request to authenticate. Please try again.");
+					return;
+				}
+				
+				//Perform a request to /oauth/token to get the authentication token
+				var address = BASE_URL + "/oauth/token";
+				var queryParams = {
+					code: request.query.code,
+					client_id: CLIENT_ID,
+					client_secret: CLIENT_SECRET,
+					grant_type: "authorization_code",
+					redirect_uri: CALLBACK_URL
+				};
+
+				request({
+					url: address,
+					qs: queryParams,
+					method: 'POST'
+					}, function(error, response, body){
+						if(error){
+							try{ 
+								logErr(JSON.stringify(error)); 
+							} catch (e) {}
+							result.redirect("/error");
+						} else {
+							var codeResponse = JSON.parse(body);
+							var token = codeResponse.access_token;
+
+							code = token;
+							
+							//Redirect to done
+							res.redirect("/done");
+						}			
+					});
+			});
+			app.get("/", function(request, result){
+				result.redirect("index.html");
+			});
+			app.get("/done", function(request, result){
+				result.redirect("done.html");
+			});
+			app.get("/error", function(request, result){
+				result.redirect("error.html");
+			});
+			server = app.listen(PORT, function(){
+				log("Server listening now...")
+			});
+		}
+		
+		isInited = true;
 	} else {
 		console.log("Invalid init params!");	
 	} 
-}
+};
 
 /**
   Login into Pavlok's API. Note that this relies on Node being able to listen
   on port 3000 (or a port passed in init), and Node being able to write to 
-  ./pavlok-token.json (assuming save isn't false).
+  ./pavlok-token.json (assuming save isn't false). If we're in server mode,
+  this will fail.
   
   @param {Function} callback - Callback with two arrguments. First argument 
                                is true or false depending on success/failure,
                                and the second is the auth token on success.
  **/
-exports.login = function(cId, cSecret, options, callback){
-	if(typeof cId == "function"){ //New usage; we just pass in a callback
-		callback = cId;
-	} else {
-    	//Setup from options first
-    	if(typeof options == "function"){
-			exports.init(cId, cSecret);
-			callback = options;
-		} else {
-    		exports.init(cId, cSecret, options);
-		}
-    }
+exports.login = function(callback){
+	if(!isInited){
+		logErr("You must call pavlok.init(...) first.");
+		return;
+	}
+	if(!isServer){
+		logErr("Login is invalid when running as a server!");
+		return;
+	}
+	if(siginingIn){
+		logErr("You can't login while trying to login.");
+		return;
+	}
+	
+	if(code != null){
+		log("Code loaded from disk: " + code);
+		callback(true, code);
+		return;
+	}
+	
+	open("http://localhost:" + PORT + "/auth/pavlok");
+};
+/*
 
     if(CALLBACK_URL == null){
 		CALLBACK_URL = "http://localhost:" + PORT + "/auth/pavlok/result";
@@ -225,9 +288,7 @@ exports.login = function(cId, cSecret, options, callback){
         if(SAVE) log("Unable to load code from disk; starting server...");
     }
         
-    server = app.listen(PORT, function(){
-        open("http://localhost:" + PORT + "/auth/pavlok");
-    });
+    
        
     passport.use(new OAuth2Strategy({
         authorizationURL: BASE_URL + "/oauth/authorize",
@@ -251,7 +312,7 @@ exports.login = function(cId, cSecret, options, callback){
 
         return done(null, {}); //No user object checking for Pavlok's API
     }));
-
+*/
     signingIn = true;
 }
 
