74c1bef8f2fce0831b5947f948d4713a093ced22
Maclyn Brandwein <maclyn@maclyn.me>
2016-04-21 14:27:08 -0400

    Update test.

--- test/test.js
+++ test/test.js
@@ -8,7 +8,8 @@ try {
     if(token == null) throw "Token error!";
 } catch(e) {
     console.log("You must have signed in once with the Pavlok module before, saving an auth token to disk, to test.");
-    process.exit(1);
+    console.log("Testing only implemented for the client mode.");
+	process.exit(1);
 }
 
 describe('Pavlok', function(){
@@ -16,9 +17,16 @@ describe('Pavlok', function(){
     this.slow(5000);
     this.timeout(10000);
 
-    describe('#login()', function(){
+   describe('#init()', function(){
+		it('does not do anything here', function(done){
+			pavlok.init("clientId", "clientSecret", {});
+			done();
+		});
+   });
+
+   describe('#login()', function(){
         it('should give us the auth code', function(done){
-            pavlok.login('not', 'used', { verbose: false }, function(res, code){
+            pavlok.login(function(res, code){
                 if(res)
                     done();
                 else
@@ -29,34 +37,59 @@ describe('Pavlok', function(){
 
     describe('#zap()', function(){
         it('should send a mild zap', function(done){
-            pavlok.zap(20, function(success, message){
-                if(success)
-                    done();
-                else
-                    throw message;
-            });
+            pavlok.zap({
+				"intensity": 100,	
+				"callback": function(success, message){
+					if(success)
+						done();
+					else
+						throw message;
+				}
+			});
         });
     });
 
     describe('#beep()', function(){
         it('should send a beep', function(done){
-            pavlok.beep(2, function(success, message){
-                if(success)
-                    done();
-                else
-                    throw message;
-            });
-        });
+             pavlok.beep({
+				"intensity": 127,
+				"callback": function(success, message){
+					if(success)
+						done();
+					else
+						throw message;
+				}
+			});       
+		});
     });
 
     describe('#vibrate()', function(){
         it('should send a mild vibration', function(done){
-            pavlok.vibrate(100, function(success, message){
-                if(success)
-                    done();
-                else
-                    throw message;
-            });
-        });
+            pavlok.vibrate({
+				"intensity": 100,
+				"callback": function(success, message){
+					if(success)
+						done();
+					else
+						throw message;
+				}
+			});
+		});
+    });
+
+    describe('#pattern()', function(){
+        it('should send a beep-vibrate-beep pattern twice', function(done){
+            pavlok.vibrate({
+				"intensity": 100,
+				"pattern": [ "beep", "vibrate", "beep" ],
+				"count": 2,
+				"callback": function(success, message){
+					if(success)
+						done();
+					else
+						throw message;
+				}
+			});
+		});
     });
 });
